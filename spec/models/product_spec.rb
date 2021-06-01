dfdd45805d020312e7f474ebda3b355658659eeb
Mohamed Hassan <mhass405@gmail.com>
2021-06-01 00:46:52 +0000

    Add product-spec tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,6 +6,35 @@ RSpec.describe Product, type: :model do
     before do
       @category = Category.new(:name => "Test Category")
       @product = @category.products.new(:name => "nameOfProduct", :price => 22, :quantity => 1)
+      
+    end
+
+    it "returns an error when name is nil" do
+      @product.name = nil
+      @product.save
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include "Name can't be blank"
+    end
+
+    it "returns an error when price is nil" do
+      @product.price_cents = nil
+      @product.save
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include "Price can't be blank"
+    end
+
+    it "returns an error when quantity is nil" do
+      @product.quantity = nil
+      @product.save
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include "Quantity can't be blank"
+    end
+  
+    it "returns an error when category is nil" do
+      @product2 = Product.create({name: "burner",price: 123, quantity: 5, category: nil})
+      @product2.save
+      expect(@product2).to_not be_valid
+      expect(@product2.errors.full_messages).to include "Category can't be blank"
     end
   end
 end
